6b1c194e1402a3fe19754cdce51f06df114a4715
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-08 12:34:08 +0300

    Initial Load

---
+++ LeetCode2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Queue<int> q = new Queue<int> ();
+
+			q.Enqueue (1);
+			q.Enqueue (2);
+			q.Enqueue (3);
+			q.Enqueue (4);
+
+		
+		}
+	}
+}
---
+++ LeetCode2/StackFromQueues.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode2
+{
+	public class StackFromQueues
+	{
+		Queue<int> q1 ;
+		Queue<int> q2 ;
+
+		public StackFromQueues()
+		{
+			q1 = new Queue<int> ();
+			q2 = new Queue<int> ();
+		}
+
+		public StackFromQueues ()
+		{
+		}
+
+		public void Push(int x) {
+
+		}
+
+		// Removes the element on top of the stack.
+		public void Pop() {
+
+		}
+
+		// Get the top element.
+		public int Top() {
+			return 0;
+		}
+
+		// Return whether the stack is empty.
+		public bool Empty() {
+			return false;
+		}
+	}
+}
+
