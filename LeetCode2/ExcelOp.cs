e34c12d05235a48fea782298c355f6b285df881f
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-08 20:02:41 +0300

    Convert int to excel title

---
+++ LeetCode2/ExcelOp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode2
+{
+	public class ExcelOp
+	{
+		public string ConvertToTitle(int n) {
+
+			Stack<char> s = new Stack<char> ();
+
+			while (n > 0) 
+			{
+				n--;
+				int remainder = n % 26;
+				s.Push ((char)(65 + remainder));
+				n = (int) n / 26;
+			}
+
+			return string.Join("",s.ToList());
+		}
+	}
+}
+
--- LeetCode2/Program.cs
+++ LeetCode2/Program.cs
@@ -8,35 +8,14 @@ namespace LeetCode2
 	{
 		public static void Main (string[] args)
 		{
-			ListNode n1 = new ListNode (1);
-			ListNode n2 = new ListNode (2);
-			ListNode n3 = new ListNode (3);
-			ListNode n4 = new ListNode (4);
-			ListNode n5 = new ListNode (5);
-			ListNode n6 = new ListNode (5);
-			ListNode n7 = new ListNode (5);
+			ExcelOp o = new ExcelOp ();
+			Console.WriteLine (o.ConvertToTitle (1));
 
-			n1.next = n2;
-			n2.next = n3;
-			n3.next = n4;
-			n4.next = n5;
-			n5.next = n6;
-			n6.next = n7;
-
-			ListOperations o = new ListOperations ();
-			var head = o.DeleteDuplicates (n1);
-
-			var tmp = head;
-
-			while (tmp != null) {
-				Console.WriteLine (tmp.val);
-				tmp = tmp.next;
-			}
 		}
 
 		public static bool IsPowerOfTwo(int n)
 		{
-			if (n <= 0) {
+			if (n <= 0) {	
 				return false;
 			}
 			if (((n - 1) & n) == 0) {
