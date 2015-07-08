7615c015d2031b32dc658f1aaddfeaf516689ef5
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-08 13:32:16 +0300

    Stack from queues

--- LeetCode2/Program.cs
+++ LeetCode2/Program.cs
@@ -8,14 +8,15 @@ namespace LeetCode2
 	{
 		public static void Main (string[] args)
 		{
-			Queue<int> q = new Queue<int> ();
-
-			q.Enqueue (1);
-			q.Enqueue (2);
-			q.Enqueue (3);
-			q.Enqueue (4);
-
-		
+			StackFromQueues q = new StackFromQueues ();
+			q.Push (1);
+			q.Push (2);
+			q.Push (3);
+			q.Push (4);
+			q.Pop ();
+			q.Pop ();
+			q.Push (10);
+			Console.WriteLine (q.Top());
 		}
 	}
 }
--- LeetCode2/StackFromQueues.cs
+++ LeetCode2/StackFromQueues.cs
@@ -8,34 +8,56 @@ namespace LeetCode2
 	{
 		Queue<int> q1 ;
 		Queue<int> q2 ;
+		int count;
 
 		public StackFromQueues()
 		{
+			count = 0;
 			q1 = new Queue<int> ();
 			q2 = new Queue<int> ();
 		}
 
-		public StackFromQueues ()
+		public void Push(int x) 
 		{
-		}
-
-		public void Push(int x) {
-
+			count++;
+			if (q1.Count == 0) {
+				q1.Enqueue (x);
+				while (q2.Count > 0) {
+					q1.Enqueue (q2.Dequeue ());
+				}
+			} else {
+				q2.Enqueue (x);
+				while (q1.Count > 0) {
+					q2.Enqueue (q1.Dequeue ());
+				}
+			}
 		}
 
 		// Removes the element on top of the stack.
 		public void Pop() {
-
+			if (!Empty ()) {
+				count--;
+				if (q1.Count == 0) {
+					q2.Dequeue ();
+				} else {
+					q1.Dequeue ();
+				}
+			}
 		}
 
 		// Get the top element.
 		public int Top() {
-			return 0;
+			if (q1.Count == 0) {
+				return q2.Peek ();
+			} else {
+				
+				return q1.Peek ();
+			}
 		}
 
 		// Return whether the stack is empty.
 		public bool Empty() {
-			return false;
+			return count == 0;
 		}
 	}
 }
